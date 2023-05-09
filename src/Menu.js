bae97098dba470c96b53481154f76810fe4fc7d3
Shalini Panwar <shalini.panwar89@gmail.com>
2023-05-09 12:43:13 -0400

    changes to fix loading of default meals in menu

--- src/Menu.js
+++ src/Menu.js
@@ -83,10 +83,21 @@ function Menu() {
 
   // Using the useEffect hook to load saved meals
   useEffect(() => {
-    const storedItems = JSON.parse(localStorage.getItem("meals")) || []; // if there is no saved item, an empty array is returned
-    setMeals(storedItems); // updating the state with saved items
+    const storedItems = JSON.parse(localStorage.getItem("meals")) || [];
+    if (storedItems.length > 0) {
+      setMeals(storedItems);
+    } else {
+      setMeals([["MONDAY", "Poha & boiled eggs", "Daal-rice & yogurt", "Daal-spinach khichri"],
+        ["TUESDAY", "Oats-chia-seed pudding", "Sambhar-dosa & chutney", "Paneer scramble & roti"],
+        ["WEDNESDAY", "Egg sandwich & sprouts", "Rajma-rice & raita", "Chicken curry & roti"],
+        ["THURSDAY", "Daal chilla & fruit", "Chicken curry & roti", "Veggie noodles & soup"],
+        ["FRIDAY", "French toast & smoothie", "Mixed veg-roti & raita", "Egg curry & rice"],
+        ["SATURDAY", "Besan chilla and smoothie", "Matar paneer & roti", "Pasta & tomato soup"],
+        ["SUNDAY", "Paneer paratha & sprouts", "Chole-rice & raita", "Soya pulav & soup"],
+      ]);
+    }
   }, []);
-
+  
   // Function to handle editing of a meal
   const handleEditMeal = (mealToBeEdited) => {
     // to initiate prompt for user to enter the changed meal
